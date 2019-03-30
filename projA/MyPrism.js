b697e50e5854f3129373244636cfe34ebe5ea88f
Carlos Eduardo <cnova1998@gmail.com>
2019-03-30 10:16:10 +0000

    Added normals to the prism

--- projA/MyPrism.js
+++ projA/MyPrism.js
@@ -13,7 +13,7 @@ class MyPrism extends CGFobject {
         let ang = 0;
         let alphaAng = 2*Math.PI/this.slices;
 
-        for (let i = 0; i < this.slices; i++) {
+        for (let i = 0; i < this.slices*2; i++) {
             let sa = Math.sin(ang);
             let saa = Math.sin(ang + alphaAng);
             let ca = Math.cos(ang);
@@ -23,10 +23,34 @@ class MyPrism extends CGFobject {
             this.vertices.push(ca, 0, sa);
             this.vertices.push(ca, 1, sa);
 
+            // Normals
+            // triangle normal computed by cross product of two edges
+            var normal= [
+                saa-sa,
+                ca*saa-sa*caa,
+                caa-ca
+            ];
+
+            // normalization
+            var nsize=Math.sqrt(
+                normal[0]*normal[0]+
+                normal[1]*normal[1]+
+                normal[2]*normal[2]
+                );
+            normal[0]/=nsize;
+            normal[1]/=nsize;
+            normal[2]/=nsize;
+
+            // push normal once for each vertex of this triangle
+            this.normals.push(...normal);
+            this.normals.push(...normal);
+            this.normals.push(...normal);
+            // /Normals
+
             // Can't connect two faces if it's the first iteration
             if (i == 0) continue;
 
-            if (i == 1) this.indices.push(1,0,2,1,2,3);
+            if (i == 1) this.indices.push(2,0,1,3,2,1);
 
             else {
                 this.indices.push(
@@ -38,8 +62,15 @@ class MyPrism extends CGFobject {
                     this.indices[6*(i-2)+5]+2
                 )
             }
+
             ang += alphaAng;
         }
+
+        // Duplicate vertex array
+        //this.vertices = [...this.vertices];
+
+        // Duplicate indices array
+        //this.indices = [...this.indices];
 
         this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
