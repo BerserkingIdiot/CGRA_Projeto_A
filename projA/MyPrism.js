449e044f95342937fc4ab229d7fca64bc8711671
Carlos Eduardo <cnova1998@gmail.com>
2019-03-31 13:03:31 +0100

    Prism logic overhaul

--- projA/MyPrism.js
+++ projA/MyPrism.js
@@ -1,11 +1,14 @@
+/**
+* MyPyramid
+* @constructor
+*/
 class MyPrism extends CGFobject {
     constructor(scene, slices) {
         super(scene);
         this.slices = slices;
-        this.initBuffers(scene);
+        this.initBuffers();
     }
-
-    initBuffers(scene) { 
+    initBuffers() {
         this.vertices = [];
         this.indices = [];
         this.normals = [];
@@ -13,22 +16,26 @@ class MyPrism extends CGFobject {
         let ang = 0;
         let alphaAng = 2*Math.PI/this.slices;
 
-        for (let i = 0; i < this.slices*2; i++) {
-            let sa = Math.sin(ang);
-            let saa = Math.sin(ang + alphaAng);
-            let ca = Math.cos(ang);
-            let caa = Math.cos(ang + alphaAng);
+        for(let i = 0; i < this.slices; i++){
+            // All vertices have to be declared for a given face
+            // even if they are shared with others, as the normals 
+            // in each face will be different
 
-            // Declare vertices
-            this.vertices.push(ca, 0, sa);
-            this.vertices.push(ca, 1, sa);
+            let sa=Math.sin(ang);
+            let saa=Math.sin(ang+alphaAng);
+            let ca=Math.cos(ang);
+            let caa=Math.cos(ang+alphaAng);
 
-            // Normals
+            this.vertices.push(ca, 0, -sa);
+            this.vertices.push(caa, 0, -saa);
+            this.vertices.push(ca, 1, -sa);
+            this.vertices.push(caa, 1, -saa);
+
             // triangle normal computed by cross product of two edges
             let normal= [
-                saa-sa, // meter -2*ang
+                Math.cos(ang + (alphaAng/2)),
                 0,
-                caa-ca,
+                -Math.sin(ang + (alphaAng/2)),
             ];
 
             // normalization
@@ -45,65 +52,17 @@ class MyPrism extends CGFobject {
             this.normals.push(...normal);
             this.normals.push(...normal);
             this.normals.push(...normal);
-            // /Normals
+            this.normals.push(...normal);
 
-            // Can't connect two faces if it's the first iteration
-            if (i == 0) continue;
+            this.indices.push(4*i, (4*i+1), (4*i+2), (4*i+3), (4*i+2), (4*i+1));
 
-            if (i == 1) this.indices.push(2,0,1,3,2,1);
-
-            else {
-                this.indices.push(
-                    this.indices[6*(i-2)]+2,
-                    this.indices[6*(i-2)+1]+2,
-                    this.indices[6*(i-2)+2]+2,
-                    this.indices[6*(i-2)+3]+2,
-                    this.indices[6*(i-2)+4]+2,
-                    this.indices[6*(i-2)+5]+2
-                )
-            }
-
-            ang += alphaAng;
-        }
-
-        // Duplicate vertex array
-        this.vertices = [...this.vertices];
-
-        // Duplicate indices array
-        this.indices = [...this.indices];
-
-        for (let i = 0; i < this.slices*2; i++) {
-            let sa = Math.sin(ang);
-            let saa = Math.sin(ang + alphaAng);
-            let ca = Math.cos(ang);
-            let caa = Math.cos(ang + alphaAng);
-
-            let normal= [
-                saa,
-                0,
-                caa,
-            ];
-
-            // normalization
-            let nsize=Math.sqrt(
-                normal[0]*normal[0]+
-                normal[1]*normal[1]+
-                normal[2]*normal[2]
-                );
-            normal[0]/=nsize;
-            normal[1]/=nsize;
-            normal[2]/=nsize;
-
-            // push normal once for each vertex of this triangle
-            this.normals.push(normal);
-            this.normals.push(normal);
-            this.normals.push(normal);
+            ang+=alphaAng;
         }
 
         this.primitiveType = this.scene.gl.TRIANGLES;
-		this.initGLBuffers();
+        this.initGLBuffers();
     }
-
+    
     updateBuffers(complexity){
         this.slices = 3 + Math.round(9 * complexity); //complexity varies 0-1, so slices varies 3-12
 
