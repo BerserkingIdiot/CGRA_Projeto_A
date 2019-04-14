a02add7b0423191d1db4fb15b27db796fdccca01
Carlos Eduardo <cnova1998@gmail.com>
2019-04-14 16:13:35 +0100

    Added MyTreeRowPatch

--- projA/MyScene.js
+++ projA/MyScene.js
@@ -25,6 +25,7 @@ class MyScene extends CGFscene {
         this.prism = new MyPrism(this, 8);
         this.cylinder = new MyCylinder(this, 8);
         this.tree = new MyTree(this, 2, 0.8, 3, 1.5);
+        this.treeGroupPatch = new MyTreeGroupPatch(this);
         this.treeRowPatch = new MyTreeRowPatch(this);
 
         //Objects connected to MyInterface
@@ -102,11 +103,10 @@ class MyScene extends CGFscene {
 
 
         this.pushMatrix();
-        //this.voxelHill.display();
-        //this.tree.display();
+        this.treeRowPatch.display()
         this.popMatrix();
 
-        this.treeRowPatch.display();
+        //this.treeGroupPatch.display();
 
         //this.prism.display();
 
--- projA/MyTreeRowPatch.js
+++ projA/MyTreeRowPatch.js
@@ -3,20 +3,18 @@ class MyTreeRowPatch extends CGFobject {
         super(scene);
         this.tree = new MyTree(scene, 2, 0.5, 1.5, 1);
         this.randomizers = [];
-        for (let i = 0; i < 9; i++) {
+        for (let i = 0; i < 6; i++) {
             let randomizer = Math.random()*(1-0.7) + 0.7;
             this.randomizers.push(randomizer);
         }
     }   
 
     display(scene) {
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                this.scene.pushMatrix();
-                this.scene.translate(i*3*this.randomizers[i+j] + 0.5, 0, j*3*this.randomizers[i+j] + 0.5);
-                this.tree.display();
-                this.scene.popMatrix();
-            }
+        for (let i = 0; i < 6; i++) {
+            this.scene.pushMatrix();
+            this.scene.translate(i*3*this.randomizers[i] + 0.7, 0, 3*this.randomizers[i] + 0.7);
+            this.tree.display();
+            this.scene.popMatrix();
         }   
     }
 }
